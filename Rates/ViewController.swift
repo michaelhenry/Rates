7959da3798600b8261b2606e005d2cc15c97b891
Michael Henry Pantaleon <me@iamkel.net>
2019-08-27 22:40:45 +0900

    initial project setup

---
+++ Rates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rates
+//
+//  Created by Michael Henry Pantaleon on 2019/08/27.
+//  Copyright © 2019 Michael Henry Pantaleon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
+
