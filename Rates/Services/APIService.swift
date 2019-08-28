162798e80bf098f8e3e7ea32049f63c267bba50f
Michael Henry Pantaleon <me@iamkel.net>
2019-08-29 01:18:52 +0900

    provide a dynamic configuration setup.

--- Rates/Services/APIService.swift
+++ Rates/Services/APIService.swift
@@ -13,17 +13,27 @@ class APIService {
   static let shared = APIService()
   private lazy var requestCaller = RequestCaller()
   
+  private var apiKey:String {
+    // I do recommend to set the PRODUCTION API KEY via CI ENVIRONMENT Variable and
+    // on Development Environment we provide a default so that
+    // the app will continue to work without any additional configuration.
+    guard let _apiKey = Bundle.main
+      .object(forInfoDictionaryKey: "API_KEY") as? String else {
+      return "0e95ff074fc3d9a7352cae4a4182224f"
+    }
+    return _apiKey
+  }
+  
   func fetchCurrencies(completion: @escaping(Result<Currencies, Error>) -> Void) {
     let request = URLRequest(
-      url: URL(string: "http://apilayer.net/api/list?access_key=0e95ff074fc3d9a7352cae4a4182224f")!)
+      url: URL(string: "http://apilayer.net/api/list?access_key=\(apiKey)")!)
     requestCaller.call(request: request, completion: completion)
   }
   
   func fetchLive(source:String = "USD", completion: @escaping(Result<Quotes, Error>) -> Void) {
     
     let request = URLRequest(
-      url: URL(string: "http://apilayer.net/api/live?access_key=0e95ff074fc3d9a7352cae4a4182224f")!)
+      url: URL(string: "http://apilayer.net/api/live?access_key=\(apiKey)")!)
     requestCaller.call(request: request, completion: completion)
   }
-  
 }
