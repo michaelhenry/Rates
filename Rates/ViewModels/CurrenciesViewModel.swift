ad9ee5667c1dbde503fc0aac63e447531ba05405
Michael Henry Pantaleon <me@iamkel.net>
2019-09-01 08:27:47 +0900

    use the onError closure instead of printing it.

--- Rates/ViewModels/CurrenciesViewModel.swift
+++ Rates/ViewModels/CurrenciesViewModel.swift
@@ -127,7 +127,7 @@ extension CurrenciesViewModel {
       try fetchedResultsController.performFetch()
       onReloadVisibleData?()
     } catch {
-      print("ERROR \(error)")
+      onError?(error)
     }
   }
 }
